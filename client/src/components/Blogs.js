f7f5810a5b187ff6df0b496f2ec8039c7eece51e
Bramjot Singh <151504705+bramjot14@users.noreply.github.com>
2024-11-05 01:00:09 -0500

    Update Blogs.js

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -8,7 +8,7 @@ const Blogs = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await axios.get('http://localhost:5002/api/blogs');
+        const response = await axios.get('https://real-estate-pyvy.onrender.com/api/blogs');
         setBlogs(response.data);
       } catch (error) {
         console.error('Error fetching blogs:', error);
