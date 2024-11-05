23ab6ef9d992c9e94af751cebccb5f4e4a651cbc
Bramjot Singh <151504705+bramjot14@users.noreply.github.com>
2024-11-04 20:35:00 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-// db.js
-const pgp = require('pg-promise')();
-const db = pgp({
-    host: 'localhost',
-    port: 5432,
-    database: 'real_estate_app',
-    user: 'postgres',
-    password: 'Chauhan@123'
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'real_estate_app_user',
+  host: 'dpg-cskmm9t6l47c73blvse0-a',
+  database: 'real_estate_app',
+  password: 'sE0uq4H9kHCd1Q3zE6injaNrd1CdCzaU',
+  port: 5432,
 });
 
-module.exports = db;  // Export the db instance
+module.exports = pool;
