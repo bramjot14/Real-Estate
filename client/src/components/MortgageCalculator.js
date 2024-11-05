fbdde2cdf73da8a4bec5966ab4a8617e5608c7e5
Bramjot Singh <151504705+bramjot14@users.noreply.github.com>
2024-11-05 01:13:38 -0500

    Update MortgageCalculator.js

--- client/src/components/MortgageCalculator.js
+++ client/src/components/MortgageCalculator.js
@@ -4,7 +4,7 @@ const MortgageCalculator = () => {
   return (
     <div style={{ padding: '20px' }}>
       <iframe
-        src="http://localhost:5002/mortgage-calculator" // Adjust this URL to point to your backend's EJS route
+        src="https://real-estate-pyvy.onrender.com/mortgage-calculator" // Adjust this URL to point to your backend's EJS route
         style={{
           width: '100%',
           height: '1000px', // Adjust the height as per your content
