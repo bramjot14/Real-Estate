79d545e7ebd46043ae32c3390dd76fc8143bc39b
Bramjot Singh <151504705+bramjot14@users.noreply.github.com>
2024-11-05 00:48:31 -0500

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'real_estate_app_user',
   host: 'dpg-cskmm9t6l47c73blvse0-a',
-  database: 'real_estate_app',
+  database: 'real_estate_app_render',
   password: 'sE0uq4H9kHCd1Q3zE6injaNrd1CdCzaU',
   port: 5432,
   ssl: {
