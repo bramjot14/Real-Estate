46f0bde0a246a042b1672e8eb6a9618b649cdfd8
Bramjot Singh <151504705+bramjot14@users.noreply.github.com>
2024-11-07 14:02:19 -0500

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import "./Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faFacebook, faApple, faTwitter, faEnvelope } from '@fortawesome/free-brands-svg-icons';
 
 const NavigationBar = ({ user, setUser }) => {
+      const [showModal, setShowModal] = useState(false);
+
+    const handleLoginClick = () => {
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+    };
   return (
     <Navbar bg="dark" expand="lg" collapseOnSelect className="custom-navbar">
       <Container>
@@ -42,13 +53,45 @@ const NavigationBar = ({ user, setUser }) => {
               </>
             ) : (
               <>
-                <Nav.Link href="https://real-estate-pyvy.onrender.com/auth/google" className="nav-link-custom">Login with Google</Nav.Link>
+                <Button className="nav-link-custom" onClick={handleLoginClick}>Login</Button> 
+                // <Nav.Link href="https://real-estate-pyvy.onrender.com/auth/google" className="nav-link-custom">Login with Google</Nav.Link>
                 <Nav.Link as={Link} to="/signup" className="nav-link-custom">Sign Up</Nav.Link>
               </>
             )}
           </Nav>
         </Navbar.Collapse>
       </Container>
+{/* Modal for Sign-In Options */}
+            <Modal
+    show={showModal}
+    onHide={handleClose}
+    className="custom-modal"
+    backdrop="static" /* Use the static backdrop to ensure proper blur effect */
+>
+    <button className="custom-close-button" onClick={handleClose}>&times;</button>
+    <Modal.Header className="custom-modal-header">
+        <Modal.Title>Welcome back.</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        {/* Sign-In Buttons with Icons */}
+        <a href="https://real-estate-pyvy.onrender.com/auth/google" className="custom-auth-button">
+            <FontAwesomeIcon icon={faGoogle} className="auth-icon" />
+            Sign in with Google
+        </a>
+        <a href="https://real-estate-pyvy.onrender.com/auth/facebook" className="custom-auth-button">
+            <FontAwesomeIcon icon={faFacebook} className="auth-icon" />
+            Sign in with Facebook
+        </a>
+        <a href="https://real-estate-pyvy.onrender.com/auth/twitter" className="custom-auth-button">
+            <FontAwesomeIcon icon={faTwitter} className="auth-icon" />
+            Sign in with Twitter
+        </a>
+        {/* <a href="mailto:example@example.com" className="custom-auth-button">
+            <FontAwesomeIcon icon={faEnvelope} className="auth-icon" />
+            Sign in with Email
+        </a> */}
+    </Modal.Body>
+</Modal>
     </Navbar>
   );
 };
