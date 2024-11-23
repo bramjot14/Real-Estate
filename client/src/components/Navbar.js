596dea2b3f788453cd7b30cb2339f99d8374860e
Bramjot Singh <151504705+bramjot14@users.noreply.github.com>
2024-11-23 13:53:32 -0500

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { Navbar, Nav, Container, Button, Modal } from 'react-bootstrap';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 import "./Navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle, faFacebook, faApple, faTwitter, faEnvelope } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const NavigationBar = ({ user, setUser }) => {
-      const [showModal, setShowModal] = useState(false); 
-        const [expanded, setExpanded] = useState(false); // Add state for Navbar expansion
+    const [showModal, setShowModal] = useState(false);
+    const [expanded, setExpanded] = useState(false); // State for Navbar expansion
 
-    const handleLoginClick = () => { 
+    const handleLoginClick = () => {
         setShowModal(true);
     };
 
@@ -18,90 +18,91 @@ const NavigationBar = ({ user, setUser }) => {
     };
 
     const handleNavClick = () => {
-        setExpanded(false); // Close the navbar when a link is clicked
+        setExpanded(false); // Collapse the navbar
     };
-  return (
-    <Navbar bg="dark" expand="lg" collapseOnSelect className="custom-navbar" expanded={expanded} // Control the Navbar expansion>
-      <Container>
-        <Navbar.Brand as={Link} to="/" className="navbar-brand-custom">
-          Singh's Realty
-        </Navbar.Brand>
 
-        {/* Toggle button for mobile view */}
-       <Navbar.Toggle
+    return (
+        <Navbar
+            bg="dark"
+            expand="lg"
+            collapseOnSelect
+            className="custom-navbar"
+            expanded={expanded} // Controlled by state
+        >
+            <Container>
+                <Navbar.Brand as={Link} to="/" className="navbar-brand-custom">
+                    Singh's Realty
+                </Navbar.Brand>
+
+                {/* Toggle button for mobile view */}
+                <Navbar.Toggle
                     aria-controls="navbar-nav"
                     onClick={() => setExpanded(expanded ? false : true)} // Toggle navbar expansion
                 />
-        <Navbar.Collapse id="navbar-nav">
-          {/* Centered main links */}
-          <Nav className="mx-auto custom-nav-links">
-            <Nav.Link as={Link} to="/" onClick={handleNavClick}>Home</Nav.Link>
-            <Nav.Link as={Link} to="/mortgage-calculator" onClick={handleNavClick}>Mortgage Calculator</Nav.Link>
-            <Nav.Link as={Link} to="/blogs" onClick={handleNavClick}>Blogs</Nav.Link>
-            <Nav.Link as={Link} to="/schedule" onClick={handleNavClick}>Schedule Appointment</Nav.Link>
-            <Nav.Link as={Link} to="/about" onClick={handleNavClick}>About Us/Our Team</Nav.Link>
-            <Nav.Link as={Link} to="/property-listings" onClick={handleNavClick}>Property Listings</Nav.Link>
-            <Nav.Link as={Link} to="/admin/dashboard" className="nav-link-custom" onClick={handleNavClick}>Admin Dashboard</Nav.Link>
-          </Nav>
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="mx-auto custom-nav-links">
+                        <Nav.Link as={Link} to="/" onClick={handleNavClick}>Home</Nav.Link>
+                        <Nav.Link as={Link} to="/mortgage-calculator" onClick={handleNavClick}>Mortgage Calculator</Nav.Link>
+                        <Nav.Link as={Link} to="/blogs" onClick={handleNavClick}>Blogs</Nav.Link>
+                        <Nav.Link as={Link} to="/schedule" onClick={handleNavClick}>Schedule Appointment</Nav.Link>
+                        <Nav.Link as={Link} to="/about" onClick={handleNavClick}>About Us/Our Team</Nav.Link>
+                        <Nav.Link as={Link} to="/property-listings" onClick={handleNavClick}>Property Listings</Nav.Link>
+                        {user && user.role === 'admin' && (
+                            <Nav.Link as={Link} to="/admin/dashboard" onClick={handleNavClick}>
+                                Admin Dashboard
+                            </Nav.Link>
+                        )}
+                    </Nav>
 
-          {/* Right-aligned user-related links */}
-          <Nav className="ml-auto custom-nav-auth">
-            {user ? (
-              <>
-                <Nav.Link as={Link} to="/profile" className="nav-link-custom" onClick={handleNavClick}>Profile</Nav.Link>
-                {user.role === 'admin' && 
-                  (
-                  <>
-                    {/* <Nav.Link as={Link} to="/admin/reviews" className="nav-link-custom">Manage Reviews</Nav.Link> */}
-                    <Nav.Link as={Link} to="/admin/dashboard" className="nav-link-custom" onClick={handleNavClick}>Admin Dashboard</Nav.Link>
-                  </>
-                )}
-                <Nav.Link href="https://real-estate-pyvy.onrender.com/logout" className="nav-link-custom" >Logout</Nav.Link>
-              </>
-            ) : (
-              <>
-                <Button className="nav-link-custom" onClick={handleLoginClick}>Login</Button> 
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-{/* Modal for Sign-In Options */}
+                    <Nav className="ml-auto custom-nav-auth">
+                        {user ? (
+                            <>
+                                <Nav.Link as={Link} to="/profile" onClick={handleNavClick}>Profile</Nav.Link>
+                                <Nav.Link href="https://real-estate-pyvy.onrender.com/logout">
+                                    Logout
+                                </Nav.Link>
+                            </>
+                        ) : (
+                            <Button className="nav-link-custom" onClick={handleLoginClick}>
+                                Login
+                            </Button>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+
+            {/* Modal for Sign-In Options */}
             <Modal
-    show={showModal}
-    onHide={handleClose}
-    className="custom-modal"
-    backdrop="static" /* Use the static backdrop to ensure proper blur effect */
->
-    <button className="custom-close-button" onClick={handleClose}>&times;</button>
-    <Modal.Header className="custom-modal-header">
-        <Modal.Title>Welcome back.</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-        {/* Sign-In Buttons with Icons */}
-        <a href="https://real-estate-pyvy.onrender.com/auth/google" className="custom-auth-button">
-            <FontAwesomeIcon icon={faGoogle} className="auth-icon" />
-            Sign in with Google
-        </a>
-        <a href="https://real-estate-pyvy.onrender.com/auth/facebook" className="custom-auth-button">
-            <FontAwesomeIcon icon={faFacebook} className="auth-icon" />
-            Sign in with Facebook
-        </a>
-        <a href="https://real-estate-pyvy.onrender.com/auth/twitter" className="custom-auth-button">
-            <FontAwesomeIcon icon={faTwitter} className="auth-icon" />
-            Sign in with Twitter
-        </a>
-        {/* <a href="mailto:example@example.com" className="custom-auth-button">
-            <FontAwesomeIcon icon={faEnvelope} className="auth-icon" />
-            Sign in with Email
-        </a> */}
-    </Modal.Body>
-</Modal>
-    </Navbar>
-  );
+                show={showModal}
+                onHide={handleClose}
+                className="custom-modal"
+                backdrop="static"
+            >
+                <button className="custom-close-button" onClick={handleClose}>&times;</button>
+                <Modal.Header className="custom-modal-header">
+                    <Modal.Title>Welcome back.</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <a href="https://real-estate-pyvy.onrender.com/auth/google" className="custom-auth-button">
+                        <FontAwesomeIcon icon={faGoogle} className="auth-icon" />
+                        Sign in with Google
+                    </a>
+                    <a href="https://real-estate-pyvy.onrender.com/auth/facebook" className="custom-auth-button">
+                        <FontAwesomeIcon icon={faFacebook} className="auth-icon" />
+                        Sign in with Facebook
+                    </a>
+                    <a href="https://real-estate-pyvy.onrender.com/auth/twitter" className="custom-auth-button">
+                        <FontAwesomeIcon icon={faTwitter} className="auth-icon" />
+                        Sign in with Twitter
+                    </a>
+                </Modal.Body>
+            </Modal>
+        </Navbar>
+    );
 };
 
 export default NavigationBar;
+
 
 
 
